ebcb4e4c9bc448edb26685b300d990411f0ced81
Davide Saccardo <davide.saccardo14@gmail.com>
2022-11-21 00:09:09 +0100

    Update segmentation.py

--- segmentation.py
+++ segmentation.py
@@ -90,10 +90,7 @@ def run(
         # Visualize the results: craters with mask
         if view_img:
             cv2.imshow('img',out.get_image()[:, :, ::-1])
-            cv2.waitKey(0) #CHECK THISSSSS
-
-        # Remove the extension .jpg to write on the csv file and save the mask
-        get_filename_without_extension(imageName)
+            #cv2.waitKey(0) #CHECK THISSSSS
       
         # Convert the mask to a binary image (a matrix with False and True)
         p = outputs['instances'].pred_masks.cpu().numpy()
